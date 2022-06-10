4466a264ca37be83716a6074dba12419e3e08f98
alexodisseus <alexodisseus@gmail.com>
2022-06-10 17:36:05 -0300

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.widget import Widget
 from kivy.lang import Builder
 
 from kivymd.app import MDApp
@@ -8,9 +9,13 @@ from kivymd.uix.screen import Screen
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDFillRoundFlatButton
 
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
+
 
 
 class Inicial(Screen):
+    
     pass 
 class Menu(Screen):
     pass 
@@ -22,29 +27,23 @@ class CafeCereja_App(MDApp):
     def __init__(self, *args):
         super(CafeCereja_App, self).__init__(*args)
 
-        self.window = Opcao()
-        
 
 
     def build(self):
         self.theme_cls.primary_palette = "Purple" #, "Red"
         self.theme_cls.primary_hue = "800"  # "500"
         
-        self.window.add_widget(Menu())
-        self.window.add_widget(Inicial())
 
 
-        print(self.window)
 
-        return self.window
+        return Opcao()
 
     def proxima_fase(self):
         print("asd")
-        self.window.current = 'inicial'
+
     def anterior_fase(self):
         print("asd")
-        self.window.current = 'menu'
-        
+       
 
 
 CafeCereja_App().run()
