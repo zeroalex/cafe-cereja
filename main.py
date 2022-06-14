c9b5a687958fbf376da0f7aa357d0709654d994c
alexodisseus <alexodisseus@gmail.com>
2022-06-14 16:49:11 -0300

    first commit

--- main.py
+++ main.py
@@ -12,15 +12,20 @@ from kivymd.uix.button import MDFillRoundFlatButton
 from kivy.graphics import Rectangle
 from kivy.graphics import Color
 
+
+from kivy.core.window import Window
+from kivymd.uix.filemanager import MDFileManager
+from kivymd.toast import toast
+
 import csv
 
-with open('data/dados.csv','r', encoding="ISO-8859-1") as f:
+
+#trecho deve ser apagado ao implementar banco de dados
+with open("data/dados.csv" ,'r', encoding="ISO-8859-1") as f:
     reader = csv.reader(f)
 
-    
+            
     lista=[''.join(x).split(';') for x in reader]
-    
-    
 
 
         
@@ -32,8 +37,13 @@ class Tela_canvas(Widget):
     def __init__(self, **kwargs):
         super(Tela_canvas, self).__init__(**kwargs)
 
+        #introduzir banco de dados
+        #self.lista = madruguinha.carregar_empresas()
+
+        #apagar trecho abaixo
+        self.lista = lista
         with self.canvas:
-            for x in lista:
+            for x in self.lista:
                 
                 if x[7]!='banca_numero':
                     
@@ -43,18 +53,90 @@ class Tela_canvas(Widget):
                         Color(1,0,0,1 , mode='rgba')
                     
                     self.rect = Rectangle(pos = (int(x[7]),int(x[7])), size=(10,10) )
-
-
+    def buscar(self):
+        print("asdasdasdasd")
+        
 
     pass
 
-class Inicial(Screen):
+class Tela_Mapa(Screen):
+
+
+    pass 
+
+class Carregar(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Window.bind(on_keyboard=self.events)
+        self.manager_open = False
+        self.path = ''
+        self.file_manager = MDFileManager(
+            exit_manager=self.exit_manager,
+            select_path=self.select_path,
+            preview=True,
+        )
+    
+
+    def file_manager_open(self):
+        self.file_manager.show('../')  # output manager to the screen
+        self.manager_open = True
+
+    def select_path(self, path):
+        self.path = path
+
+        self.exit_manager()
+        toast(path)
+
+    def exit_manager(self, *args):
+        '''Called when the user reaches the root of the directory tree.'''
+
+        self.manager_open = False
+        self.file_manager.close()
+
+    def events(self, instance, keyboard, keycode, text, modifiers):
+        '''Called when buttons are pressed on the mobile device.'''
+
+        if keyboard in (1001, 27):
+            if self.manager_open:
+                self.file_manager.back()
+        return True
+    def atualizar(self):
+
+        with open(self.path+"/dados.csv" ,'r', encoding="ISO-8859-1") as f:
+            reader = csv.reader(f)
+
+            
+            lista=[''.join(x).split(';') for x in reader]
+            
+        #mudar para atualizar banco de dados
+        print(lista)
+
     
     pass 
 class Menu(Screen):
     pass 
+class Busca(Screen):
+
+    pass 
 class Opcao(ScreenManager):
-    pass
+    def __init__(self, **kwargs):
+        super(Opcao, self).__init__(**kwargs)
+        self.dados = []
+    
+    def buscar(self, entrada):
+        print('asdas')
+        print(entrada)
+        
+    def menu_carregar(self):
+        self.transition.direction = 'right'
+        self.current="menu"
+    def busca_carregar(self):
+        self.transition.direction = 'up'
+        self.current="busca"
+    def voltar_mapa(self):
+        self.transition.direction = 'down'
+        self.current="mapa"
+
     
 class Lista_empresas(MDList):
     def __init__(self, **kwargs):
@@ -68,7 +150,7 @@ class Lista_empresas(MDList):
 
 
 class CafeCereja_App(MDApp):
-    lista_bancas = lista
+    #lista_bancas = lista
     def __init__(self, *args):
         super(CafeCereja_App, self).__init__(*args)
         
@@ -85,6 +167,7 @@ class CafeCereja_App(MDApp):
         
         pass
 
+    
     def anterior_fase(self):
         print("asd")
        
